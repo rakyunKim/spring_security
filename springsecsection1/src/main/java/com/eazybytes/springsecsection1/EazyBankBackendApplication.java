b13bd922d0f72e9f580b33693f27e511170de5c7
rakyun1 <dbskim234@naver.com>
2024-12-09 20:54:38 +0900

    init

---
+++ springsecsection1/src/main/java/com/eazybytes/springsecsection1/EazyBankBackendApplication.java
@@ -0,0 +1,14 @@
+package com.eazybytes.springsecsection1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazyBankBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(com.eazybytes.springsecsection1.EazyBankBackendApplication.class, args);
+    }
+
+}
+
